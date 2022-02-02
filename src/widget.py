396b0251e7143bc17dc9fe439c0f3cef8df2f67c
Nikita <nikita.zinkevich@yandex.ru>
2022-02-02 17:36:07 +0300

    Make table work and stretch

--- src/widget.py
+++ src/widget.py
@@ -3,9 +3,8 @@ import sys
 import sqlite3
 
 from datetime import datetime
-from os import curdir
 
-from PySide6.QtWidgets import QWidget, QFileDialog, QTableWidgetItem, QAbstractItemView
+from PySide6.QtWidgets import QWidget, QFileDialog, QTableWidgetItem, QHeaderView
 from PySide6.QtCore import Slot
 
 from form import Ui_Widget
@@ -33,11 +32,15 @@ class Widget(QWidget):
 
         self.ui.tableWidget.setColumnCount(2)
         self.ui.tableWidget.setHorizontalHeaderLabels(["Name", "Date"])
+        self.ui.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.ui.tableWidget.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
         for title, raw_date in cur:
             if title is not None and raw_date is not None:
                 date = datetime.fromtimestamp(round(raw_date/1000000))
                 key = self.ui.tableWidget.rowCount()
+                self.ui.tableWidget.setRowCount(key+1)
+                print(key)
                 title_item = QTableWidgetItem(title)
                 date_item = QTableWidgetItem(str(date))
                 self.ui.tableWidget.setItem(key, 0, title_item)
