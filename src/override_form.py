925bfb57d64587d3737a9b764a545e8e8bb3d2a4
Nikita <nikita.zinkevich@yandex.ru>
2022-02-04 11:29:36 +0300

    Add hidden column for urls

--- src/override_form.py
+++ src/override_form.py
@@ -3,7 +3,6 @@ from PySide6.QtWidgets import QAbstractItemView, QHeaderView
 
 def overrideTableWidget(ui):
     ui.lineEdit.setText("/usr/firefox/comp/places.sqlite_")
-    ui.tableWidget.setColumnCount(2)
     ui.tableWidget.setHorizontalHeaderLabels(["Name", "Date"])
     ui.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
     ui.tableWidget.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
--- src/widget.py
+++ src/widget.py
@@ -5,7 +5,7 @@ import sqlite3
 from override_form import overrideTableWidget
 from datetime import datetime
 
-from PySide6.QtWidgets import QWidget, QFileDialog, QTableWidgetItem, QHeaderView, QAbstractItemView
+from PySide6.QtWidgets import QWidget, QFileDialog, QTableWidgetItem
 from PySide6.QtCore import Slot, Qt
 
 from form import Ui_Widget
@@ -22,6 +22,7 @@ class Widget(QWidget):
         self.ui.select_button.clicked.connect(self.show_dialog)
         self.ui.exit_button.clicked.connect(sys.exit)
         self.ui.confirm_button.clicked.connect(self.get_history)
+        self.ui.tableWidget.itemDoubleClicked.connect(self.open_url)
 
 # Taking history from db and output to table widget
     @Slot()
@@ -29,19 +30,24 @@ class Widget(QWidget):
         path = self.ui.lineEdit.text()
         con = sqlite3.connect(path)
         cur = con.cursor()
-        cur.execute("SELECT title, last_visit_date AS date FROM moz_places "
+        cur.execute("SELECT title, last_visit_date, url AS date FROM moz_places "
                     "WHERE title IS NOT Null AND last_visit_date IS NOT NULL")
         table_content = cur.fetchall()
         cur.close()
-        data_length = len(table_content)
-        self.ui.tableWidget.setRowCount(data_length)
+        row_count = len(table_content)
+        column_count = len(table_content[0])
+        self.ui.tableWidget.setRowCount(row_count)
+        self.ui.tableWidget.setColumnCount(column_count)
+        self.ui.tableWidget.setColumnHidden(2, True)
 
-        for row_number, (title, raw_date) in enumerate(table_content):
+        for row_number, (title, raw_date, site_url) in enumerate(table_content):
             date = datetime.fromtimestamp(round(raw_date/1000000))
             title_item = QTableWidgetItem(title)
             date_item = QTableWidgetItem(str(date))
+            url_item = QTableWidgetItem(site_url)
             self.ui.tableWidget.setItem(row_number, 0, title_item)
             self.ui.tableWidget.setItem(row_number, 1, date_item)
+            self.ui.tableWidget.setItem(row_number, 2, url_item)
 
         self.ui.tableWidget.sortItems(1, Qt.AscendingOrder)
 
@@ -49,3 +55,7 @@ class Widget(QWidget):
     def show_dialog(self):
         path = QFileDialog.getOpenFileName()[0]
         self.ui.lineEdit.setText(path)
+
+    @Slot()
+    def open_url(self):
+        self.ui.tableWidget.currentItem()
