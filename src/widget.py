cdaa427dfe42420669f9354ce4021ccf2fb756b1
Nikita <nicita.zinkevitch@yandex.ru>
2022-02-21 21:48:35 +0300

    Add plot to stats tab

--- src/widget.py
+++ src/widget.py
@@ -108,16 +108,6 @@ class Widget(QWidget):
             self.ui.activityTable.setItem(i, 0, hour_item)
             self.ui.activityTable.setItem(i, 1, count_item)
 
-
-class MainWindow(QMainWindow):
-
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-
-        # Create the maptlotlib FigureCanvas object,
-        # which defines a single set of axes as self.axes.
         sc = MplCanvas(self, width=5, height=4, dpi=100)
         sc.axes.plot([0, 1, 2, 3, 4], [10, 1, 20, 3, 40])
-        self.setCentralWidget(sc)
-
-        self.show()
+        self.ui.formLayout_2.addWidget(sc)
